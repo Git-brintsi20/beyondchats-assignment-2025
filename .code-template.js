0cbdcb358a803fd3039dfa24db58b74ed8ec92b2
S_Harshita_B <163279064+Git-brintsi20@users.noreply.github.com>
2025-12-29 13:19:30 +0530

    Add code template for BeyondChats assignment

---
+++ .code-template.js
@@ -0,0 +1,23 @@
+/**
+ * ============================================================================
+ * BEYONDCHATS ASSIGNMENT PROJECT
+ * ============================================================================
+ * 
+ * File: [FILENAME]
+ * Author: [SALUGU HARSHITA BHANU]
+ * Email: [shiki2hustle@gmail.com]
+ * Created: [29-12-25]
+ * 
+ * Repository: https://github.com/[username]/beyondchats-assignment-2024
+ * 
+ * Copyright (c) 2025 [Your Name]. All rights reserved.
+ * 
+ * This code is part of an assignment submission for BeyondChats.
+ * Commercial use without explicit permission is prohibited.
+ * 
+ * License: MIT with Attribution (see LICENSE file)
+ * 
+ * ============================================================================
+ */
+
+// YOUR CODE STARTS HERE
