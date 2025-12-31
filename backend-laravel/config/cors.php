7c0613ea0c27155f76f0cf26e90292386fcdc647
S_Harshita_B <shiki2hustle@gmail.com>
2025-12-31 17:16:09 +0530

    Update CORS to allow all paths including reset route

--- backend-laravel/config/cors.php
+++ backend-laravel/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'], // Added '*' to allow the reset route
 
     'allowed_methods' => ['*'],
 
